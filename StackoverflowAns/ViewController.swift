2b70979655931a4008bc7bdf1972a471e15b4c4f
sauvik dolui <sauvikdolui@gmail.com>
2020-08-18 00:06:35 +0530

    Initial commmit

---
+++ StackoverflowAns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackoverflowAns
+//
+//  Created by Sauvik Dolui on 16/08/20.
+//  Copyright © 2020 Sauvik Dolui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Target2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Target2
+//
+//  Created by Sauvik Dolui on 17/08/20.
+//  Copyright © 2020 Sauvik Dolui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
